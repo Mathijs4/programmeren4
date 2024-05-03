efb4ad0b620df4faced23694c640e810f9f58829
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-05-03 17:47:15 +0200

    added post request for user

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 let database = [];
 let id = 0;
@@ -15,6 +18,27 @@ app.get('/', (req, res) => {
   res.status(200).json({
     status: 200,
     message: 'Hello World!',
+  });
+});
+
+app.post('/api/user', (req, res) => {
+  let user = req.body;
+  id++;
+
+  console.log('User:', user);
+
+  user = {
+    id,
+    ...user,
+  };
+  
+  database.push(user);
+  console.log('Database:', database)
+
+  res.status(201).json({
+    status: 201,
+    message: 'User created',
+    user,
   });
 });
 
